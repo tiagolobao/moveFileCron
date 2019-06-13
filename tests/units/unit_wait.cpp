b8c947d61b2e28ae2b0b538e3753aa96d360c375
Tiago Britto Lobão <tiago.blobao@gmail.com>
2019-06-13 14:48:30 -0300

    <Tests> Improve wait test

--- tests/units/unit_wait.cpp
+++ tests/units/unit_wait.cpp
@@ -2,8 +2,28 @@
 #include "unit_tests.hpp"
 
 void tests::wait(){
+
+  /* Generate random wait value */
   int testValue = get_random_int(5);
   std::cout << "The test value is: " << testValue << std::endl;
-  mvc::wait(testValue);
+
+  time_t before, after;
+
+  try{
+
+    /* Wait */
+    time(&before);
+    mvc::wait(testValue);
+    time(&after);
+
+    /* Calc difference between hours */
+    int waitedTime = difftime(after, before);
+    if( waitedTime != testValue )
+      throw std::runtime_error(std::to_string(waitedTime));
+
+  } catch( std::runtime_error & e ){
+  std::cout << "Error on wait => Time: " << e.what() << std::endl;
+  }
+
   std::cout << "Done!" << std::endl;
 }
