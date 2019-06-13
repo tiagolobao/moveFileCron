4e44f4db91faaa20e0fc371d52414ea922560268
Tiago Britto Lobão <tiago.blobao@gmail.com>
2019-06-13 16:27:51 -0300

    <Tests> Invert test order for quick tests
    - Wait test will be the last one now

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -5,14 +5,14 @@ int main(){
 
   std::cout << "Starting tests..." << std::endl;
 
-  std::cout << std::endl << "mvc::wait() function test" << std::endl;
-  tests::wait();
-
   std::cout << std::endl << "mvc::move() function test" << std::endl;
   tests::move();
 
   std::cout << std::endl << "mvc::getFileList() function test" << std::endl;
   tests::getFileList();
+
+  std::cout << std::endl << "mvc::wait() function test" << std::endl;
+  tests::wait();
 
   std::cout << std::endl << " TEST DONE!" << std::endl;
   return 0;
